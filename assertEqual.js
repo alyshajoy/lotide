1256dd10fdb3edc7a232cd7511b99dac97e00328
Alysha Huot <alyshajhuot@gmail.com>
2023-10-17 15:21:34 -0600

    complete assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,8 @@
+const assertEqual = function(actual, expected) {
+  actual === expected ? console.log("😍😍😍" + " Assertion Passed: " + actual + " === " + expected) : console.log("😡😡😡" + " Assertion Failed: " + actual + " !== " + expected);
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(20, "horse");
+assertEqual("hi", "hi");
