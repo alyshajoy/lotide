11babc40c74dbf1ba1c0326d655e773e21e87551
Alysha Huot <alyshajhuot@gmail.com>
2023-10-30 20:52:21 -0600

    refactor code to properly call in functions from other files

--- assertEqual.js
+++ assertEqual.js
@@ -2,7 +2,4 @@ const assertEqual = function(actual, expected) {
   actual === expected ? console.log(`😍😍😍 Assertion Passed: ${actual} === ${expected}`) : console.log(`😡😡😡 Assertion Failed: ${actual} !== ${expected}`);
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(20, "horse");
-assertEqual("hi", "hi");
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,12 +1,8 @@
-const assertEqual = function(actual, expected) {
-  actual === expected ? console.log(`😍😍😍 Assertion Passed: ${actual} === ${expected}`) : console.log(`😡😡😡 Assertion Failed: ${actual} !== ${expected}`);
-};
+const assertEqual = require('./assertEqual');
 
 const head = function(array){
   firstElement = array.shift();
   return firstElement;
 }
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]),5);
+module.exports = head;
