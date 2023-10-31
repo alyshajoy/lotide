c0ae123622a1da434f13533f25075a4041caea6f
Alysha Huot <alyshajhuot@gmail.com>
2023-10-30 21:39:40 -0600

    fixed bug in eqArrays function

--- eqArrays.js
+++ eqArrays.js
@@ -4,16 +4,29 @@ const isEqual = function(actual, expected) {
 
 // console.log(`😍😍😍 Assertion Passed: ${actual} === ${expected}`) : console.log(`😡😡😡 Assertion Failed: ${actual} !== ${expected}`)
 
+// const eqArrays = function(array1, array2) {
+//   for (let i = 0; i < array1.length; i++) {
+//     if (isEqual(array1[i], array2[i]) === false) {
+//       return false;
+//     }
+//     if (i === 2 && isEqual(array1[i], array2[i]) === true){
+//       return true;
+//     }
+//   }
+// }
 const eqArrays = function(array1, array2) {
-  for (let i = 0; i < array1.length; i++) {
-    if (isEqual(array1[i], array2[i]) === false) {
+
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  for(let i = 0; i < array1.length; i++) {
+    if(array1[i] !== array2[i]) {
       return false;
     }
-    if (i === 2 && isEqual(array1[i], array2[i]) === true){
-      return true;
-    }
   }
-}
 
+  return true;
+}
 
 module.exports = eqArrays;
--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -5,3 +5,5 @@ console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
 
 console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
 console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
+
+console.log(eqArrays([12], [12])); // => true
