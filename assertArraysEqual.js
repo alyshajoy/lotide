e6f3310024deadd7230a89941942e57108ccc2d8
Alysha Huot <alyshajhuot@gmail.com>
2023-10-30 22:01:50 -0600

    completed refactoring middle function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -8,10 +8,4 @@ const assertArraysEqual = function(array1, array2) {
   }
 }
 
-console.log(assertArraysEqual([1, 2, 3], [1, 2, 3])) // => true
-console.log(assertArraysEqual([1, 2, 3], [3, 2, 1])) // => false
-
-console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", "3"])) // => true
-console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", 3])) // => false
-
-console.log(assertArraysEqual([12], [12]))
+module.exports = assertArraysEqual;
