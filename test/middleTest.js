4bde6417dce56db04b75266099a7387c723b8ef1
Alysha Huot <alyshajhuot@gmail.com>
2023-10-31 08:18:15 -0600

    refactor middleTest to use mocha/chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,30 @@
-const assertArraysEqual = require("../assertArraysEqual");
+const assert = require("chai").assert;
 const middle = require("../middle");
 
-assertArraysEqual(middle([1]), []); // => []
-assertArraysEqual(middle([1, 2]), []); // => []);
-assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
+describe("#middle", () => {
+
+  it("returns [] if given an array with only one item", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+
+  it("returns [2] if given [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+
+  it("returns [] if given an array of 2 items", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+
+  it("returns [3] if given [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+
+  it("returns [2, 3] if given [1, 2, 3, 4", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+
+  it("returns [3, 4] if given [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+
+});
