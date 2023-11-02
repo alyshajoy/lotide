016f277956b41af130429f593139e01dd36bc97a
Alysha Huot <alyshajhuot@gmail.com>
2023-11-01 20:25:20 -0600

    refactor eqArrays function and its test file

--- eqArrays.js
+++ eqArrays.js
@@ -1,19 +1,3 @@
-const isEqual = function(actual, expected) {
-  return actual === expected;
-};
-
-// console.log(`😍😍😍 Assertion Passed: ${actual} === ${expected}`) : console.log(`😡😡😡 Assertion Failed: ${actual} !== ${expected}`)
-
-// const eqArrays = function(array1, array2) {
-//   for (let i = 0; i < array1.length; i++) {
-//     if (isEqual(array1[i], array2[i]) === false) {
-//       return false;
-//     }
-//     if (i === 2 && isEqual(array1[i], array2[i]) === true){
-//       return true;
-//     }
-//   }
-// }
 const eqArrays = function(array1, array2) {
 
   if (array1.length !== array2.length) {
--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,9 +1,22 @@
 let eqArrays = require("../eqArrays");
+const assert = require("chai").assert;
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+describe("#eqArrays", () => {
 
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
+  it("returns true when passed two identical arrays", () => {
+    assert.deepEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  });
 
-console.log(eqArrays([12], [12])); // => true
+  it("returns false when passed two arrays with same values but in different order", () => {
+    assert.deepEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+  });
+
+  it("returns false when passed arrays with similar values, but of different types", () => {
+    assert.deepEqual(eqArrays([1, 2, 3], ["1", "2", "3"]), false);
+  });
+
+  it("returns true if passed identical arrays with only one value", () => {
+    assert.deepEqual(eqArrays([12], [12]), true);
+  });
+
+});
